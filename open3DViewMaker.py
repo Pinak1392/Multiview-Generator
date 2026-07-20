7d710793ebf48a3976fe77aa6889e7d5028dfe81
Pinak Limaye <limayepinak@gmail.com>
2026-07-20 17:42:07 +1000

    Added script

---
+++ open3DViewMaker.py
@@ -0,0 +1,23 @@
+import open3d as o3d
+import numpy as np
+
+mesh = o3d.io.read_triangle_mesh("C:/Users/limay/Downloads/Erofoichthys/Erofoichthys-iso130.ply")
+mesh.compute_vertex_normals()
+
+def generate_random_rotation(mesh):
+    # Generate a random rotation matrix
+    R = mesh.get_rotation_matrix_from_xyz(np.random.rand(3) * 2 * np.pi)
+    # Apply the transformation to the mesh
+    mesh.rotate(R, center=mesh.get_center())
+    return mesh
+
+vis = o3d.visualization.Visualizer()
+vis.create_window("3D Mesh Perspectives", width=600, height=600, left=0, top=0)
+vis.get_render_option().background_color = [0, 0, 0]
+view_control = vis.get_view_control()
+for _ in range(5):
+    vis.add_geometry(generate_random_rotation(mesh))
+    random_translation = ((np.random.rand(3) * 1000 - 500) + mesh.get_center())  # Random translation
+    view_control.set_lookat(random_translation)
+    vis.capture_screen_image(f"perspective_{_}.png",True)
+    vis.clear_geometries()
