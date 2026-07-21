35f112ea37edad94291b49dc5d10a01cbee7cee8
Pinak Limaye <limayepinak@gmail.com>
2026-07-21 13:14:59 +1000

    More efficient rotation and translation. However poses the problem of being unable to have an upside down camera

--- open3DViewMaker.py
+++ open3DViewMaker.py
@@ -10,20 +10,22 @@ args = parser.parse_args()
 mesh = o3d.io.read_triangle_mesh(args.input)
 mesh.compute_vertex_normals()
 
-def generate_random_rotation(mesh):
-    # Generate a random rotation matrix
-    R = mesh.get_rotation_matrix_from_xyz(np.random.rand(3) * 2 * np.pi)
-    # Apply the transformation to the mesh
-    mesh.rotate(R, center=mesh.get_center())
-    return mesh
-
 vis = o3d.visualization.Visualizer()
 vis.create_window("3D Mesh Perspectives", width=600, height=600, left=0, top=0)
 vis.get_render_option().background_color = [0, 0, 0]
 view_control = vis.get_view_control()
+vis.add_geometry(mesh)
 for _ in range(args.views):
-    vis.add_geometry(generate_random_rotation(mesh))
     random_translation = ((np.random.rand(3) * 1000 - 500) + mesh.get_center())  # Random translation
+    theta = np.random.rand() * 2 * np.pi
+    phi = np.arccos(2 * np.random.rand() - 1)
+    random_rotation = np.array([
+        np.sin(phi) * np.cos(theta),
+        np.sin(phi) * np.sin(theta),
+        np.cos(phi)
+    ])
+    
+    view_control.set_front(random_rotation)
     view_control.set_lookat(random_translation)
     vis.capture_screen_image(f"perspective_{_}.png",True)
-    vis.clear_geometries()
+    # vis.clear_geometries()
